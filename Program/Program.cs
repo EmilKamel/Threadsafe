f3c8da2e60b09032e891e181a163c694633ba7bc
EmilKamel <Emil.kloster.lindberg@gmail.com>
2020-03-22 12:47:11 +0100

    Initial commit

---
+++ Program/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    class Model
+    {
+        static readonly object ClassLockObject = new object();
+
+        static int classvar
+        {
+            get { return classvar; }
+            set
+            {
+                lock (ClassLockObject)
+                {
+                    classvar = value;
+                }
+            }
+        }
+
+        readonly object InstanceLockObject = new object();
+
+        int instanceVar
+        {
+            get { return instanceVar;}
+            set
+            {
+                lock (InstanceLockObject)
+                {
+                    instanceVar = value;
+                }
+            }
+        }
+        
+
+
+    }
+}
